5b2452bd1009476a540e80f6cbcc7a387dd10cdc
webtam81 <t.h@tammyharris.co.uk>
2023-11-02 20:42:41 +0000

    work on classes

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
+//const Employee = require("./lib/Employee"); //TODO rm
 const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+//const Engineer = require("./lib/Engineer");
+//const Intern = require("./lib/Intern");
+//const inquirer = require("inquirer");
+//const path = require("path");
+//const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+//const OUTPUT_DIR = path.resolve(__dirname, "output");
+//const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./src/page-template.js");
+//const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
@@ -27,3 +28,7 @@ const render = require("./src/page-template.js");
 
 //temporary testing
 
+const mark = new Manager('Bob',3,'e@mail.com');
+
+console.log(mark);
+console.log(mark.getRole());
