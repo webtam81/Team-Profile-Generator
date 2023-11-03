1af89c4761b9258bcfde84f1b2da414d8d7b4bda
webtam81 <t.h@tammyharris.co.uk>
2023-11-03 00:28:01 +0000

    updated classes, question arrays

--- index.js
+++ index.js
@@ -1,16 +1,73 @@
 //const Employee = require("./lib/Employee"); //TODO rm
 const Manager = require("./lib/Manager");
-//const Engineer = require("./lib/Engineer");
-//const Intern = require("./lib/Intern");
-//const inquirer = require("inquirer");
-//const path = require("path");
-//const fs = require("fs");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs/promises");
 
-//const OUTPUT_DIR = path.resolve(__dirname, "output");
-//const outputPath = path.join(OUTPUT_DIR, "team.html");
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-//const render = require("./src/page-template.js");
+const render = require("./src/page-template.js");
 
+const employeeQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Please enter name',
+        default: 'Bob'
+    }, 
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Please enter employee ID',
+        default: 12
+    }, 
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter email address',
+        default: 'e@mail.com'
+    }
+]
+
+const engineerQuestions = [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter github username',
+        default: 'gitbob'
+    }
+]
+
+const managerQuestions = [
+    {
+        type: 'input',
+        name: 'officenumber',
+        message: 'Please enter office number',
+        default: '0121 121 0121'
+    }
+]
+
+const internQuestions = [
+    {
+        type: 'input',
+        name: 'school',
+        message: 'Please enter cshool',
+        default: 'school'
+    }
+]
+
+function createManager() {
+    managerQuestionArray = [...employeeQuestions, ...managerQuestions];
+    inquirer.prompt(managerQuestionArray)
+    .then((data) => {
+        console.log(data);
+        //fs.writeFile(outputPath,render(data))
+    })
+}
+ 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
@@ -28,7 +85,9 @@ const Manager = require("./lib/Manager");
 
 //temporary testing
 
-const mark = new Manager('Bob',3,'e@mail.com');
+//const mark = new Manager('Bob',3,'e@mail.com',456456);
 
-console.log(mark);
-console.log(mark.getRole());
+//console.log(mark);
+//console.log(mark.getRole());
+
+createManager();
